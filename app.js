43e1abcbef7a3675a3b963349b314e097814e4ee
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-03-11 13:48:27 +0100

    add image and style

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const { createApp } = Vue
 const app = createApp({
   data() {
     return {
-        greatings: 'hello'
+        greatings: 'hello world.',
+        imageSrc: './img/hello-world.jpg',
+        imgWidth: 500,
+        ib: 'inline-block',
+        isActive: true,
     }
-  }
+  },
 }).mount('#app')
